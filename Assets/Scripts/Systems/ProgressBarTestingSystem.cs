fab061475b88ba24ffb6b5bd7314646eb85c8b60
Rossi <gustavodevrr@gmail.com>
2020-02-11 19:44:07 -0300

    Release v0.1.0 para pré visualização de UI
    Release contendo navegação de cenas com elementos UI básicos, que precisam de desenvolvimento gráfico.

---
+++ Assets/Scripts/Systems/ProgressBarTestingSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DungeonCrawlers.UI;
+using System.Collections;
+
+
+public class ProgressBarTestingSystem : MonoBehaviour
+{
+	public ProgressBar progressBar;
+	public Slider lerpController;
+
+	public void Start() {
+		StartCoroutine(Reload());
+		progressBar.OnCompleted += (sernder, args) => StartCoroutine(Reload());
+		lerpController.onValueChanged.AddListener((lerp) => progressBar.lerptTime = lerp);
+	}
+
+	private IEnumerator Reload() {
+		progressBar.Clear();
+		yield return new WaitForSeconds(progressBar.lerptTime);
+		progressBar.Output(1f);
+	}
+
+}
---
+++ Assets/Scripts/Systems/SceneLoaderSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using DungeonCrawlers;
+
+public class SceneLoaderSystem : MonoBehaviour
+{
+	public void LoadScene(int index) {
+		SceneLoader.LoadSceneAsync(index).allowSceneActivation = true;
+	}
+
+	public void Reload() {
+		SceneLoader.LoadSceneAsync(SceneLoader.CurrentSceneIndex).allowSceneActivation = true;
+	}
+}
