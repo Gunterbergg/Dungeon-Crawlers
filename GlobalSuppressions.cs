b268f891c451740f6f7fa543953c52a4fb9fe579
Rossi <gustavodevrr@gmail.com>
2020-03-07 15:13:25 -0300

    Adicionado tilesets temporários
    - Adicionado sprites  e tileset temporários
    - Adcionado prefabs de salas utilizante tileset temporário

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Qualidade do Código", "IDE0051:Remover membros privados não utilizados", Justification = "<Pendente>", Scope = "member", Target = "~M:DungeonCrawlers.Systems.FormRequestSystem.Awake")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE0044:Adicionar modificador somente leitura", Justification = "<Pendente>", Scope = "member", Target = "~F:DungeonCrawlers.UI.DialogBox.dialogQuery")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Qualidade do Código", "IDE0051:Remover membros privados não utilizados", Justification = "<Pendente>", Scope = "member", Target = "~M:DungeonCrawlers.Systems.PlayerMovementSystem.Awake")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Qualidade do Código", "IDE0051:Remover membros privados não utilizados", Justification = "<Pendente>", Scope = "member", Target = "~M:DungeonCrawlers.UI.TextInput.OnValidate")]
