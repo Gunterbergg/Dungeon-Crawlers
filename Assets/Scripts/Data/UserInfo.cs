94bc18f6dca993a4d199c07be1fcd5418b465d57
GustavoRossi <gustavodevrr@gmail.com>
2020-03-21 19:26:25 -0300

    Adicionado classes containers de dados de usuários
    - Adicionado classes containers de dados 'UserInfo' correspondendo as regras de negócio especificadas no levantamento de requisitos

---
+++ Assets/Scripts/Data/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DungeonCrawlers.Data
+{
+	[CreateAssetMenu(fileName = "UserInfo", menuName ="InfoContainer/UserInfo")]
+	public class UserInfo : ScriptableObject
+	{
+		public uint id;
+		public new string name;
+		public string email;
+		public int souls;
+		public int gold;
+		public int gems;
+	}
+}
