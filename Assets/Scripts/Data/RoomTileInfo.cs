69e13e7060ed4ff556574abef80eb47219ee37c4
GustavoRossi <gustavodevrr@gmail.com>
2020-03-21 19:43:26 -0300

    Adicionado prefabs e scriptable objects para disponibilizara dados de construção e informação de usuário
    - Adicionado prefabs de tiles para geração automática de salas, dividadas entre tiles de parede, tiles de chão e tiles de chão próximas a paredes
    - Adicionado scriptable objects para dados de requisição ao conjunto de salas do usuário, para guardar a coleção de salas do usuário, informações gerais do usuáro e informações de salas pré-definidas
    - Alterado sprites para conter divisão intuitiva correspondente a divisao dos prefabs das tiles.

---
+++ Assets/Scripts/Data/RoomTileInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonCrawlers.Data
+{
+	[Serializable]
+	public struct RoomTiles
+	{
+		public List<GameObject> upperLeftTiles;
+		public List<GameObject> upperRightTiles;
+		public List<GameObject> lowerRightTiles;
+		public List<GameObject> lowerLeftTiles;
+		public List<GameObject> leftTiles;
+		public List<GameObject> upperTiles;
+		public List<GameObject> rightTiles;
+		public List<GameObject> lowerTiles;
+	}
+
+	[CreateAssetMenu(fileName = "RoomTileInfo", menuName = "InfoContainer/RoomTileInfo")]
+	public class RoomTileInfo : ScriptableObject
+	{
+		public RoomTiles walls;
+		public RoomTiles wallSidedFloors;
+		public List<GameObject> floors;
+	}
+}
